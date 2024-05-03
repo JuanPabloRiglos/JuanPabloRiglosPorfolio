6869c22cf859f33efa03c89231bbd17b45de4584
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-05-03 20:37:10 -0300

    Se Agrego la ruta de experiencia, se avanzo sobre las Cards de los Proyectos, se ag, y footer

---
+++ src/mocs/projects.js
@@ -0,0 +1,43 @@
+const projects = [
+	{
+		title: "Proyecto HTMLX",
+		description: "Un proyecto HTMLX emocionante que te dejará sin aliento.",
+		link: "https://www.ejemplo.com/proyecto-htmlx",
+		github: "https://github.com/ejemplo/proyecto-htmlx",
+		tags: ["htmlx", "react.js", "tailwind"],
+		img: "/una-taza-de-javascript.png",
+		subtitle: "Pequeña explicacion del proyecto",
+	},
+	{
+		title: "Proyecto Increíble con React.js",
+		description:
+			"Un proyecto increíble desarrollado con React.js que cambiará el mundo.",
+		link: "https://www.ejemplo.com/proyecto-react",
+		github: "https://github.com/ejemplo/proyecto-react",
+		tags: ["htmlx", "react.js", "tailwind"],
+		img: "/una-taza-de-javascript.png",
+		subtitle: "Pequeña explicacion del proyecto",
+	},
+	{
+		title: "Proyecto Innovador con Tailwind",
+		description:
+			"Un proyecto innovador que aprovecha al máximo el poder de Tailwind CSS.",
+		link: "https://www.ejemplo.com/proyecto-tailwind",
+		github: "https://github.com/ejemplo/proyecto-tailwind",
+		tags: ["htmlx", "react.js", "tailwind"],
+		img: "/una-taza-de-javascript.png",
+		subtitle: "Pequeña explicacion del proyecto",
+	},
+	{
+		title: "Proyecto Impresionante con HTMLX y React.js",
+		description:
+			"Un proyecto impresionante que combina las mejores características de HTMLX y React.js.",
+		link: "https://www.ejemplo.com/proyecto-htmlx-react",
+		github: "https://github.com/ejemplo/proyecto-htmlx-react",
+		tags: ["htmlx", "react.js", "tailwind"],
+		img: "/una-taza-de-javascript.png",
+		subtitle: "Pequeña explicacion del proyecto",
+	},
+];
+
+export default projects;
