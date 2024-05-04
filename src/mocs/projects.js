c996b229e4b68206ca1862fa71611d226b97ec67
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-05-04 14:33:40 -0300

    Se inyecto info a card de proyectos y se les dio un buen acabado. Se agrego about me, se controlo la vista en celular, se rompe nuevo footer a partir de sm hasta lg. Resta seccion Contacto para la primer version terminada

--- src/mocs/projects.js
+++ src/mocs/projects.js
@@ -1,42 +1,41 @@
 const projects = [
 	{
-		title: "Proyecto HTMLX",
-		description: "Un proyecto HTMLX emocionante que te dejará sin aliento.",
+		title: "MyLibrary App",
+		description:
+			"Esta aplicacion me llena de orgullo, si bien le falta mucho diseño para que pueda ser bonita, lo cierto es que surgio de una necesidad y pudo satisfacer a la misma.",
+		bigDescription:
+			"Esta aplicacion me llena de orgullo, si bien le falta mucho diseño para que pueda ser bonita, lo cierto es que surgio de una necesidad y pudo satisfacer a la misma. Soy un consumidor constante de videos de you tube, y en cuanto a herramienta de estudio, me vi en la necesidad de poder guardar videos, y dejarme algunos comentarios. A eso le sume la posibilidad de de guardarlos en distintas listas de reproduccion, de editarlos, borrarlos y hasta la posibilidiad de logearte, y seguir a otros usuarios en una etapa ya posterior. ",
 		link: "https://www.ejemplo.com/proyecto-htmlx",
 		github: "https://github.com/ejemplo/proyecto-htmlx",
 		tags: ["htmlx", "react.js", "tailwind"],
-		img: "/una-taza-de-javascript.png",
-		subtitle: "Pequeña explicacion del proyecto",
+		img: "/projectsImgs/DosCelularesLibrary.png",
+		subtitle: "Un proyecto hecho a mi medida, y a la tuya tambien!",
 	},
 	{
-		title: "Proyecto Increíble con React.js",
+		title: "Querida Alergia",
 		description:
-			"Un proyecto increíble desarrollado con React.js que cambiará el mundo.",
-		link: "https://www.ejemplo.com/proyecto-react",
-		github: "https://github.com/ejemplo/proyecto-react",
+			"Querida Alergia surgio de un trabajo colaboratiro y si bien se encuentra en desarrollo constante, su proposito es facilitar informacion en cuanto al ambito gastronomico a todos aquellos que sufren de intolerancias y alergias alimentarias.",
+		bigDescription:
+			"Querida Alergia surgio de un trabajo colaboratiro y si bien se encuentra en desarrollo constante, su proposito es facilitar informacion en cuanto al ambito gastronomico a todos aquellos que sufren de intolerancias y alergias alimentarias. La misma nacio dentro del siclo 'No Country', donde tuvo participacion distintos equipos entre los que destacan los de diseño ui/ux, QA, Backend y Frontend, del cual formé parte y con el cual aun sigo colaborando",
+		link: "https://querida-alergia-dev.onrender.com/",
+		github: "https://github.com/No-Country/S14-03-M-Node-React",
 		tags: ["htmlx", "react.js", "tailwind"],
-		img: "/una-taza-de-javascript.png",
-		subtitle: "Pequeña explicacion del proyecto",
+		img: "/projectsImgs/NewQueridaAlergia.png",
+		subtitle:
+			"Concentrando informacion gastronomica a todos aquellos que sufren de intolerancias y alergias alimentarias",
 	},
 	{
-		title: "Proyecto Innovador con Tailwind",
+		title: "MedConnect",
 		description:
-			"Un proyecto innovador que aprovecha al máximo el poder de Tailwind CSS.",
-		link: "https://www.ejemplo.com/proyecto-tailwind",
-		github: "https://github.com/ejemplo/proyecto-tailwind",
+			"Un aplicativo realizado de manera colaborativa, pensado para acercar a pacientes y doctores favoreciendo la comunicacion inmediate, mejorando el acceso a su informacion y a la consecución de turnos",
+		bigDescription:
+			"Un aplicativo realizado de manera colaborativa, pensado para acercar a pacientes y doctores favoreciendo la comunicacion inmediate, mejorando el acceso a su informacion y a la consecución de turnos. La misma permite el logeo como paciente o doctor, renderizando informacion distinta segun el tipo de usuario logeado y las necesidades que cada uno acarrea.",
+		link: "https://65eb5c2ca77394d626f52e05--dreamy-brioche-67f49a.netlify.app/",
+		github: "https://github.com/No-Country/c16-66-ft-node",
 		tags: ["htmlx", "react.js", "tailwind"],
-		img: "/una-taza-de-javascript.png",
-		subtitle: "Pequeña explicacion del proyecto",
-	},
-	{
-		title: "Proyecto Impresionante con HTMLX y React.js",
-		description:
-			"Un proyecto impresionante que combina las mejores características de HTMLX y React.js.",
-		link: "https://www.ejemplo.com/proyecto-htmlx-react",
-		github: "https://github.com/ejemplo/proyecto-htmlx-react",
-		tags: ["htmlx", "react.js", "tailwind"],
-		img: "/una-taza-de-javascript.png",
-		subtitle: "Pequeña explicacion del proyecto",
+		img: "/projectsImgs/MeddConectReady.png",
+		subtitle:
+			"Ambicioso proyecto que busca mejorar la conección entre medicos y pacientes",
 	},
 ];
 
