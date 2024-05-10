d7051a261f77907d030b984194446a45a405f23d
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-05-10 12:24:16 -0300

    Se agrego react y se le dio vida al boton hamburgesa en celular

---
+++ src/components/HeaderFunction.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+export function HeaderFunction() {
+	const [viewNav, setViewNav] = useState(false);
+	console.log(viewNav);
+	return (
+		<header
+			className=' flex flex-col justify-center items-center transition-all duration-500
+ sm:flex-row sm:justify-between sm:p-0
+ lg:ml-5 mt-3'
+		>
+			<div
+				className={`mb-3 rounded-full overflow-hidden flex items-center justify-center
+ sm:mb-0 sm:fixed sm:top-5 sm:left-5 sm:bg-zinc-900/90 z-10
+ `}
+			>
+				<img
+					className='hover:cursor-pointer'
+					src='/misimagenes/navImgDegradeByN.png'
+					alt=''
+					onClick={() => setViewNav(!viewNav)}
+				/>
+			</div>
+
+			<nav
+				className={`z-50 w-1/2
+
+    ${!viewNav ? "hidden " : "flex "}flex-col gap-4 items-center  
+    sm:w-auto sm:flex sm:flex-row sm:gap-6 sm:h-fit sm:rounded-s-3xl sm:p-3 sm:pl-6 sm:fixed sm:top-5 sm:right-0 sm:bg-zinc-900/90
+    lg:gap-12`}
+			>
+				<a
+					className=' text-gray-500/90 text-base hover:cursor-pointer hover:text-orange-500  border-b border-transparent hover:border-gray-500 ease-in duration-600 transition-all'
+					href='#top'
+				>
+					{" "}
+					Inicio
+				</a>
+				<a
+					className=' text-gray-500/90 text-base hover:cursor-pointer hover:text-orange-500  border-b-trasnparent hover:border-b ease-in duration-600 transition-all'
+					href='#experiencia'
+				>
+					{" "}
+					Experiencia{" "}
+				</a>
+				<a
+					className=' text-gray-500/90 text-base hover:cursor-pointer hover:text-orange-500  border-b-trasnparent hover:border-b ease-in duration-600 transition-all'
+					href='#proyectos'
+				>
+					{" "}
+					Poyectos{" "}
+				</a>
+				<a
+					className=' text-gray-500/90 text-base hover:cursor-pointer hover:text-orange-500  border-b-trasnparent hover:border-b ease-in duration-600 transition-all'
+					href='#about'
+				>
+					{" "}
+					<span className='flex flex-nowrap'>Sobre mi </span>{" "}
+				</a>
+				<a
+					className=' text-gray-500/90 text-base hover:cursor-pointer hover:text-orange-500  border-b-trasnparent hover:border-b ease-in duration-600 transition-all'
+					href='#contact'
+				>
+					{" "}
+					Contacto
+				</a>
+			</nav>
+		</header>
+	);
+}
